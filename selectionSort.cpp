cfe1910c45494f8c5b7b806f950968aed3a7c6c9
tarun0714 <manipalmemories1407@gmail.com>
2024-06-26 23:29:33 +0530

    correct selection sort code

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -2,38 +2,27 @@
 
 using namespace std;
 
-void selectionSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-       
-        int minIndex = i;
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
+int main(){
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+
+    for(int i=0;i<=n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[i]){
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
             }
         }
-        
-        swap(arr[minIndex], arr[i]);
     }
-}
 
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        std::cout << arr[i] << " ";
+    for(int i=0;i<n;i++){
+        cout << arr[i] <<" " ;
     }
-    std::cout << std::endl;
-}
-
-int main() {
-    int arr[] = {64, 25, 12, 22, 11};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    
-    cout << "Original array: ";
-    printArray(arr, n);
-    
-    selectionSort(arr, n);
-    
-    cout << "Sorted array: ";
-    printArray(arr, n);
-    
-    return 0;
 }
